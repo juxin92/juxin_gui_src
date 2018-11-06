ca6e2a0f95a513f9cc8d651237b8117353b94018
juxin <1129549361@qq.com>
2018-11-06 20:16:59 +0800

    2 commit

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 class Test {
-	public static void main£¨String[] args) {
+	public static void main(String[] args) {
 		int a = 1;
 		int b = 2;
 		System.out.println(a + " + " + b + " = " + (a + b));
